6bee4e88519f6981578693d909d12ab65b3fd529
Aleksandr Badaev <badaev92aleksandr@gmail.com>
2024-03-05 09:11:05 +0100

    commit HomeWork12 02

--- src/main/java/homework12/StringProcessor.java
+++ src/main/java/homework12/StringProcessor.java
@@ -17,25 +17,25 @@ public class StringProcessor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите слово для проверки на Палиндром : ");
-        String word = scanner.nextLine();
-        reverseString(new String(word));
-        boolean checkWord = isPalindrome();
+        String normWord = scanner.next().toLowerCase().trim();
+        String revWord = reverseString(normWord); //вызываем метод reverseString(вставляем переменную введенную из сканера)
+        // И сохраняем в переменной типа String revWord. Метод reverseString перевернул нам слово и вернул в переменную
+        // String revWord
+        System.out.println(revWord);
+        boolean checkWord = isPalindrome(normWord, revWord); //Вызвали метод isPalindrome(normWord, revWord)
+        // вставляем в него два String которые он отправляет в метод где они обрабатываются, в данном случае сравниваются
         System.out.println(checkWord);
         scanner.close();
     }
 
-    static String normWord;
-    static String reverseWord;
 
-    public static String reverseString(String word) {
-        normWord = word;
-        String reverseWordNew = new StringBuilder(word).reverse().toString();
-        reverseWord = reverseWordNew;
-        return word;
+    public static String reverseString(String word) { // (word <--- normWord)(reverseString(normWord)строка 21)
+        return new StringBuilder(word).reverse().toString(); //return(возврат) возвращает нам
+        // word в новом значении (перевернутом)(reverse()) в String
     }
-
-    public static boolean isPalindrome() {
-        if (normWord.equalsIgnoreCase(reverseWord)) {
+    public static boolean isPalindrome(String normWord, String reversWord) {//метод boolean принимает два String
+                                        //isPalindrome(normWord, revWord) строка 25
+        if (normWord.equalsIgnoreCase(reversWord)) {//сравниваем два полученных слова
             return true;
         } else return false;
     }
