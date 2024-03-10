b4df78d31bf31b023dcefbf8cc0e2cb45ce00fcd
Aleksandr Badaev <badaev92aleksandr@gmail.com>
2024-03-10 22:33:55 +0100

    consulting 09.03 was added

--- src/main/java/consultation0903/FakerUtil.java
+++ src/main/java/consultation0903/FakerUtil.java
@@ -8,7 +8,7 @@ public class FakerUtil {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i<400;i++){
+        for (int i = 0; i<10;i++){
             createFakeStudent();
         }
     }
