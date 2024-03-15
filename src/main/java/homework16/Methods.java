4601e920cc3ab4415292551f65ddfc1f79fc163f
Aleksandr Badaev <badaev92aleksandr@gmail.com>
2024-03-15 11:50:27 +0100

    2 new methods created

--- src/main/java/homework16/Methods.java
+++ src/main/java/homework16/Methods.java
@@ -1,16 +1,19 @@
 package homework16;
 
 
+import My.Methods.MyMethods;
+
 /*
 Цель задания:
 Понять и применить механизм Varargs для создания функций, способных принимать переменное количество аргументов.
  */
 public class Methods {
     public static void main(String[] args) {
-        System.out.println("-----------------Task01-----------------");
-        sum();
-        System.out.println("-----------------Task02-----------------");
-        statistics();
+        sum(22, 33, 55);
+        statisticsOptionOne(1,2,3,0);
+        statisticsOptionTwo(1,2,3,0,-5);
+
+
     }
 
     /*
@@ -20,17 +23,12 @@ public class Methods {
     Продемонстрируйте работу функции, передав ей различное количество аргументов.
      */
     public static void sum(int... numberN) {
-        int counter = 0;
-        if (numberN.length == 0) {
-            System.out.println("You did not enter a value! ");
-            counter++;
-        }
         int sum = 0;
         for (int i : numberN) {
             sum += i;
-        }if (counter == 0) {
-            System.out.println("The sum of all numbers is: " + sum);
         }
+        System.out.println("The sum of all numbers is: " + sum);
+
     }
    /*
    2. Средний Уровень: Статистический Анализатор.
@@ -44,18 +42,16 @@ public class Methods {
 суммировании или при расчете статистики.
     */
 
-    public static void statistics(double... numberN) {
-            if (numberN.length == 0) {
-                System.out.println("You did not enter a value! ");
-            }
+    public static void statisticsOptionOne(double... numberN) {
+        if (numberN.length == 0) {
+            System.out.println("You did not enter a value! ");
+        } else {
             double numberMin = numberN[0];
             double numberMax = numberN[0];
             double numberAverage = 0;
-            int counter = 0;
             for (double number : numberN) {
                 if (number < 0) {
                     System.out.println("Error!!! You entered a negative value!");
-                    counter++;
                 } else {
 
                     if (number < numberMin) {
@@ -68,12 +64,57 @@ public class Methods {
                 }
             }
             numberAverage = numberAverage / numberN.length;
-            if (counter == 0) {
+            if (checkNegativeNumber(numberN)) {
                 System.out.println("The minimum value is: " + numberMin);
                 System.out.println("The maximum value is: " + numberMax);
                 System.out.println("The average value is: " + numberAverage);
             }
         }
     }
+
+    // Метод для проверки на отрицательное число.
+    public static boolean checkNegativeNumber(double... numberN) {
+        boolean negativeNumber = true;
+        for (double number : numberN) {
+            if (number < 0) {
+                negativeNumber = false;
+            }
+        }
+        return negativeNumber;
+    }
+
+    // Второй вариант решения если есть отрицательное число то мы его не учитываем в вычислениях!
+    public static void statisticsOptionTwo(double... numberN) {
+        if (numberN.length == 0) {
+            System.out.println("You did not enter a value! ");
+        } else {
+            double numberMin = numberN[0];
+            double numberMax = numberN[0];
+            double numberAverage = 0;
+            int negativeNumber = 0;
+            for (double number : numberN) {
+                if (number < 0) {
+                    System.out.println("There is a negative number! We exclude it from the Calculations");
+                    negativeNumber += 1;
+                } else {
+
+                    if (number < numberMin) {
+                        numberMin = number;
+                    }
+                    if (number > numberMax) {
+                        numberMax = number;
+                    }
+                    numberAverage += number;
+                }
+            }
+            numberAverage = numberAverage / (numberN.length - negativeNumber);
+            System.out.println("The minimum value is: " + numberMin);
+            System.out.println("The maximum value is: " + numberMax);
+            System.out.println("The average value is: " + numberAverage);
+        }
+    }
+}
+
+
 
 
