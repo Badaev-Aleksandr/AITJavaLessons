fe9fe914f2c9585f307e61451bcabb49caae35ac
Aleksandr Badaev <badaev92aleksandr@gmail.com>
2024-04-21 21:59:26 +0200

    homework25 was added

---
+++ src/main/java/lesson27/MenuItemTest.java
@@ -0,0 +1,18 @@
+package lesson27;
+
+public class MenuItemTest {
+    public static void main(String[] args) {
+        OrderStatus orderStatus = OrderStatus.PENDING;
+
+        MenuItem pizza = new MenuItem("Pizza", MenuItemType.DISH);
+        MenuItem pelmeni = new MenuItem("Pelmeni", MenuItemType.DISH);
+        MenuItem airan = new MenuItem("Airan", MenuItemType.DRINK);
+        System.out.println(pizza.toString());
+        System.out.println(pelmeni.toString());
+        System.out.println(airan.toString());
+
+        System.out.println(orderStatus.name() + " " + orderStatus.getDescription());
+
+        System.out.println(orderStatus.canCancel());
+    }
+}
---
+++ src/main/java/lesson27/MenuItemType.java
@@ -0,0 +1,6 @@
+package lesson27;
+
+public enum MenuItemType {
+    DISH, DRINK;
+
+}
