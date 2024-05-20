4e99b7f37d96194c1d3303c52c311fe49c019e04
Aleksandr Badaev <badaev92aleksandr@gmail.com>
2024-05-20 17:31:03 +0200

    lesson24 class HashMapJava was updated

--- src/main/java/lesson24/HashMapJava.java
+++ src/main/java/lesson24/HashMapJava.java
@@ -6,15 +6,15 @@ import java.util.Map;
 public class HashMapJava {
     public static void main(String[] args) {
         HashMap<String, String> hashMap = new HashMap<>();
-        hashMap.put("Audi", "Deutschland");
-        hashMap.put("BMW", "Deutschland");
-        hashMap.put("Lada", "Russland");
+        hashMap.put("Audi", "Germany");
+        hashMap.put("BMW", "Germany");
+        hashMap.put("Lada", "Russia");
 
         if(hashMap.containsKey("Audi")){
             System.out.println("Audi was found");
         }
-        if(hashMap.containsValue("Deutschland")){
-            System.out.println("Deutschland was found");
+        if(hashMap.containsValue("Germany")){
+            System.out.println("Germany was found");
         }
 
         System.out.println(hashMap.keySet());
@@ -25,17 +25,17 @@ public class HashMapJava {
 
         System.out.println(hashMap.size());
 
-        hashMap.remove("Audi","Deutschland");
+        hashMap.remove("Audi","Germany");
         System.out.println("----------for-----------");
         System.out.println(hashMap.get("BMW"));
         System.out.println("----------for-----------");
 
-       // hashMap.clear();
+       // hashMap.clear(); //Чистка hashMap
         System.out.println(hashMap.entrySet());
         //-------------for-keys---------------
         System.out.println("----------for-----------");
-        for (String aotoname: hashMap.keySet()){
-            System.out.println(hashMap.get(aotoname));
+        for (String carName: hashMap.keySet()){
+            System.out.println(hashMap.get(carName));
         }
         System.out.println("----------for-values----------");
         //-------------for-values---------------
@@ -43,9 +43,9 @@ public class HashMapJava {
             System.out.println(country);
         }
 
-        // for-keys and for-values
+        // for-keys ------ and -------- for-values
         for(Map.Entry<String, String> entry : hashMap.entrySet()){
-            System.out.println("name --> " + entry.getKey() + " coutry--> " + entry.getValue());
+            System.out.println("name --> " + entry.getKey() + " country--> " + entry.getValue());
         }
     }
 
