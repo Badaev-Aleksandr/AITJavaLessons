19c39212fec257b40451314da8597c79a8f5fca9
Aleksandr Badaev <badaev92aleksandr@gmail.com>
2024-05-20 17:28:49 +0200

    homework28 class ArtGalleryTest was updated

--- src/main/java/homework28/ArtGalleryTest.java
+++ src/main/java/homework28/ArtGalleryTest.java
@@ -3,12 +3,13 @@ package homework28;
 public class ArtGalleryTest {
     public static void main(String[] args) {
         ArtGallery artworks = new ArtGallery();
-        Artwork paintingOne = new Painting( "Painting" ,"Watercolor", "45x90 cm", "Badaev");
+        Painting paintingOne = new Painting( "Painting" ,"Watercolor", "45x90 cm", "Badaev");
         Artwork paintingTwo = new Painting( "Painting","Oil", "155x200 cm", "Musterman");
         Artwork paintingThree = new Painting("Painting","Spray", "200x300 cm", "Zigel");
         Artwork sculptureOne = new Sculpture("Sculpture","Stone", 230.4, "Musterman");
         Artwork sculptureTwo = new Sculpture("Sculpture","Gold", 111.7, "Badaev");
         Artwork sculptureThree = new Sculpture("Sculpture","Silver", 567.6, "Badaev");
+
         artworks.addArtwork(sculptureThree);
         artworks.addArtwork(paintingOne);
         artworks.addArtwork(paintingTwo);
