9c8668e4266efc15909ba5fa67ef66e88efa844c
Aleksandr Badaev <badaev92aleksandr@gmail.com>
2024-05-28 10:02:06 +0200

    lesson35 was added

---
+++ src/main/java/lesson35/Zoo.java
@@ -0,0 +1,26 @@
+package lesson35;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Zoo {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Zoo.class);
+    Map<String, Animal> animalMap = new HashMap();
+
+    public void addAnimal(Animal animal) {
+        if (animal==null){
+            LOGGER.error("Ошибка при добавлении животного, равно null");
+        }else {
+            animalMap.put(animal.getName(), animal);
+        }
+    }
+
+    public void checkAnimals() {
+        for (Animal animal : animalMap.values()) {
+            LOGGER.info("Имя животного {}. Его энергия {} ", animal.getName(), animal.getEnergy());
+        }
+    }
+}
