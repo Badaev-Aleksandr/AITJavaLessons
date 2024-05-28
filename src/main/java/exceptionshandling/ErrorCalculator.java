02ccf0c3c9b39ea9d41be713552bc84aa09e75ba
Aleksandr Badaev <badaev92aleksandr@gmail.com>
2024-05-28 10:01:25 +0200

    lesson35 was added

---
+++ src/main/java/exceptionshandling/ErrorCalculator.java
@@ -0,0 +1,44 @@
+package exceptionshandling;
+import lesson35.Animal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ErrorCalculator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ErrorCalculator.class);
+    private static Animal animal = null;
+
+    public static void main(String[] args) {
+       divide(10, 2);
+      //  badMethod();
+
+
+    }
+    static void badMethod(){
+        LOGGER.info("I'm bad method");
+        badMethod();
+    }
+
+    public static void divide(int a, int b) {
+
+        int result = 0;
+
+        try {
+             result = a / b;
+             animal.getEnergy();
+
+        }catch (ArithmeticException exception){
+            LOGGER.error("Divide by zero");
+            LOGGER.error(exception.getLocalizedMessage());
+        }catch (NullPointerException nullPointerException){
+            LOGGER.error("Animal is null");
+            LOGGER.error(nullPointerException.getLocalizedMessage());
+        }
+        catch (Exception exception){
+            LOGGER.error("An unknown exception occurred");
+            LOGGER.error(exception.getLocalizedMessage());
+        }
+        LOGGER.info("a = {}, b = {}, Result: {}", a, b, result);
+        LOGGER.info("Divide was finished");
+    }
+
+}
