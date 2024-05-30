39789bad88e4e27494ad6d3455f7cf2ed517c858
Aleksandr Badaev <badaev92aleksandr@gmail.com>
2024-05-30 22:39:03 +0200

    homework 36 was added

--- src/main/java/homework36/Main.java
+++ src/main/java/homework36/Main.java
@@ -5,14 +5,15 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         StringValidator stringValidator = new StringValidator();
 
         try {
             stringValidator.StringValidator("Hell");
-        }catch (InvalidStringLengthException exception){
+        } catch (InvalidStringLengthException exception) {
             LOGGER.error(exception.getMessage());
-        }finally {
+        } finally {
             LOGGER.info("Завершение программы");
         }
     }
