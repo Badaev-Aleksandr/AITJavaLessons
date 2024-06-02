ff70ec149c32c3b261b612f46efc9a267b7ba17b
Aleksandr Badaev <badaev92aleksandr@gmail.com>
2024-06-02 19:51:31 +0200

    lesson 37 was added

--- src/main/java/lesson29/Artwork.java
+++ src/main/java/lesson29/Artwork.java
@@ -1,4 +1,4 @@
-package lesson29.homework;
+package lesson29;
 
 public interface Artwork {
 }
---
+++ src/main/test/lesson37/CalculatorTest.java
@@ -0,0 +1,21 @@
+package lesson37;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalculatorTest {
+    @Test
+    void testDivideByZero() {
+        Calculator calculator = new Calculator();
+        assertThrows(ArithmeticException.class, () -> {
+            calculator.divide(20, 0);
+        });
+    }
+    @Test
+    void testDivideByOne(){
+        Calculator calculator = new Calculator();
+        assertEquals(2,calculator.divide(20,10));
+    }
+}
