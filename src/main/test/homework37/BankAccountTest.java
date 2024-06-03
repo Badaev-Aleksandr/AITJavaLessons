fcfe8a8d4d7967699a9d8c7169c971325a42e13c
Aleksandr Badaev <badaev92aleksandr@gmail.com>
2024-06-03 21:57:10 +0200

    homework 37 was added

--- src/main/test/homework37/BankAccountTest.java
+++ src/main/test/homework37/BankAccountTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -17,26 +19,44 @@ public class BankAccountTest {
         bankAccount = new BankAccount();
     }
 
+    //Проверка на добавления ноля к балансу
     @Test
-    void testExceptionDeposit() {
-        // Проверка на добавления ноля к балансу
+    void testIllegalArgumentExceptionDeposit() {
         assertThrows(IllegalArgumentException.class, () -> {
             bankAccount.deposit(0
             );
         });
     }
 
+    //Проверка баланса на добавление суммы
     @Test
-    void testExceptionWithdraw() {
+    void testDepositPositive() {
         bankAccount.deposit(1000);
-        //Проверка на снятие суммы больше чем на счету
-        assertThrows(InsufficientFundsException.class, () -> {
-            bankAccount.withdraw(2000);
-        });
+        assertEquals(1000, bankAccount.getBalance());
+    }
 
-        // Проверка на снятие ноля с баланса
+    //Проверка баланса после снятия суммы
+    @Test
+    void testWithdrawPositive() throws InsufficientFundsException {
+        bankAccount.deposit(2000);
+        bankAccount.withdraw(1000);
+        assertEquals(1000,bankAccount.getBalance());
+    }
+
+    //Проверка на снятие ноля с баланса
+    @Test
+    void testIllegalArgumentExceptionWithdraw() {
         assertThrows(IllegalArgumentException.class, () -> {
             bankAccount.withdraw(0);
+        });
+    }
+
+    //Проверка на снятие суммы больше чем на счету
+    @Test
+    void testInsufficientFundsExceptionWithdraw() {
+        bankAccount.deposit(1000);
+        assertThrows(InsufficientFundsException.class, () -> {
+            bankAccount.withdraw(2000);
         });
     }
 
@@ -53,16 +73,17 @@ public class BankAccountTest {
     @ParameterizedTest
     @ValueSource(strings = {"123456789O", "l234567890", "123456789l"})
     void testBankAccountConstructorException(String input) {
-        assertThrows(NumberFormatException.class, ()->{
+        assertThrows(NumberFormatException.class, () -> {
             new BankAccount(input);
         });
     }
+
     //Проверка конструктора на исключение количества чисел должно быть 10
     @ParameterizedTest
     @ValueSource(strings = {"1564jdd", "5k9ahd599", "6456565ddddd"})
-    void testBankAccountConstructorIllegalArgumentException(String input){
-        assertThrows(IllegalArgumentException.class, ()-> {
-           new BankAccount(input);
+    void testBankAccountConstructorIllegalArgumentException(String input) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BankAccount(input);
         });
     }
 }
